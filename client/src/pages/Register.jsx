f9eefcfbd789fb895383e062b99fd37d944dc540
venkythota469 <thotavenki366@gmail.com>
2024-12-01 09:33:07 +0530

    Update Register.jsx

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -16,7 +16,7 @@ const Register = () => {
   if ( document.domain === "localhost" ) {
     __URL__ = "http://localhost:1337";
   } else {
-    __URL__ = "https://music-player-app-backend-yq0c.onrender.com";
+    __URL__ = "https://music-1-4j25.onrender.com";
   }
 
   const handleSubmit = async (e) => {
