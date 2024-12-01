d47363d53f6b445ba14a2ced358a77fe9e51d089
venkythota469 <thotavenki366@gmail.com>
2024-12-01 09:35:51 +0530

    Update Login.jsx

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -17,7 +17,7 @@ const Login = () => {
   if ( document.domain === "localhost" ) {
     __URL__ = "http://localhost:1337";
   } else {
-    __URL__ = "https://music-player-app-backend-yq0c.onrender.com";
+    __URL__ = "https://music-1-4j25.onrender.com";
   }
 
   const handleSubmit = async (e) =>{
